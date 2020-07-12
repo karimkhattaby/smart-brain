eadf14e95e1604c93059ee0589521f9e0dfae3da
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-12 10:54:03 +0400

    Created ImageLinkForm Component & Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 // @ts-nocheck
 import React from 'react';
-import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
       <Logo />
-      {/* <ImageLinkForm />
-      <FaceRecognition /> */}
+      <ImageLinkForm />
+      {/* <FaceRecognition /> */}
     </div>
   );
 }
