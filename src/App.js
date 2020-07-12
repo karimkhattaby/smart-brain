d9d0beaed445842cc2b05450b2f20ac4940e217d
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-12 10:37:14 +0400

    Created Logo Component & Updated App.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@
 import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
 
 function App() {
   return (
     <div className="App">
       <Navigation />
-      {/* <Logo />
-      <ImageLinkForm />
+      <Logo />
+      {/* <ImageLinkForm />
       <FaceRecognition /> */}
     </div>
   );
