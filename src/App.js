cbaad42b0a575206641f4983957dcb1e1b1ef844
karimkhattaby <18755153+karimkhattaby@users.noreply.github.com>
2020-07-12 16:19:45 +0400

    Added react-particles-js Library & Updated App.js & App.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 // @ts-nocheck
 import React from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import './App.css';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
 function App() {
   return (
     <div className="App">
+      <Particles className="particles" params={particlesOptions} />
       <Navigation />
       <Logo />
       <Rank />
